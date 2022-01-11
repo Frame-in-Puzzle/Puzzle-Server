8e4c5a5496bcf0354b54babf28118277e9201011
노경준 <developerjun0615@gmail.com>
2022-01-11 17:35:09 +0900

    [ADD] Purpose enum 초기 설정

---
+++ src/main/java/com/server/Puzzle/domain/board/enumType/Purpose.java
@@ -0,0 +1,9 @@
+package com.server.Puzzle.domain.board.enumType;
+
+// 게시글 목적
+public enum Purpose {
+    PROJECT,        // 프로젝트
+    COMPETITION,    // 대회
+    SERVICE,        // 서비스
+    STUDY           // 스터디
+}
