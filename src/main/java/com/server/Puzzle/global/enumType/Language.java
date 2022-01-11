5c40dccf4de876ccbf780abefecc2cd7657d4e6b
노경준 <developerjun0615@gmail.com>
2022-01-11 17:35:45 +0900

    [ADD] Language 엔티티 초기 설정

---
+++ src/main/java/com/server/Puzzle/global/enumType/Language.java
@@ -0,0 +1,11 @@
+package com.server.Puzzle.global.enumType;
+
+// 언어
+public enum Language {
+    JS , TS , REACT, VUE, NEXT,                             // 프론트엔드
+    JAVA, SPRING, SPRINGBOOT, GO, DJANGO, EXPRESS, NEST,    // 백엔드
+    PYTHON, C, CPP,                                         // 임베디드
+    PYTORCH, TENSORFLOW,                                    // 인공지능
+    UNITY, CSHARP, UNREALENGINE,                            // 게임
+    KOTLIN                                                  // 안드로이드
+}
