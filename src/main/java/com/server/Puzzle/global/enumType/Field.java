99b9f7e9d81e10ca001b48d7e0708e09cee59a69
노경준 <developerjun0615@gmail.com>
2022-01-11 17:33:13 +0900

    [ADD] Field enum 타입 초기 설정

---
+++ src/main/java/com/server/Puzzle/global/enumType/Field.java
@@ -0,0 +1,12 @@
+package com.server.Puzzle.global.enumType;
+
+// 분야
+public enum Field {
+    FRONTEND,   // 프론트
+    BACKEND,    // 백엔드
+    EMBEDDED,   // 임베디드
+    AI,         // 인공지능
+    GAME,       // 게임
+    IOS,        // iOS
+    ANDROID     // 안드로이드
+}
