9236bc650d746df34ea28eef02b1d506b5a6ece3
노경준 <developerjun0615@gmail.com>
2022-01-11 17:30:43 +0900

    [Update] User User 엔티티 초기 설정

--- src/main/java/com/server/Puzzle/domain/user/domain/User.java
+++ src/main/java/com/server/Puzzle/domain/user/domain/User.java
@@ -1,6 +1,7 @@
 package com.server.Puzzle.domain.user.domain;
 
 import com.server.Puzzle.global.entity.BaseTimeEntity;
+import com.server.Puzzle.global.enumType.Field;
 
 import javax.persistence.*;
 
@@ -11,13 +12,22 @@ public class User extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_name", nullable = false)
-    private String name;
-
     @Column(name = "user_email", nullable = false)
     private String email;
 
+    @Column(name = "user_name", nullable = false)
+    private String name;
+
+    @Column(name = "user_field", nullable = false)
+    private Field field;
+
+    @Column(name = "user_introduce", nullable = false)
+    private String introduce;
+
+    @Column(name = "user_url", nullable = false)
+    private String url;
+
     @Column(name = "user_image_url", nullable = false)
-    private String image;
+    private String imageUrl;
 
 }
