406df57c86ee80aea3e94be6240a1873dc711add
hyunin <hyunin0102@gmail.com>
2022-01-24 22:15:56 +0900

    (JwtTokenFilterConfiguer) create

---
+++ src/main/java/com/server/Puzzle/global/config/security/jwt/JwtTokenFilterConfiguer.java
@@ -0,0 +1,21 @@
+package com.server.Puzzle.global.config.security.jwt;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenFilterConfiguer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final JwtTokenProvider jwtTokenProdvider;
+
+    public JwtTokenFilterConfiguer(JwtTokenProvider jwtTokenProdvider) {
+        this.jwtTokenProdvider = jwtTokenProdvider;
+    }
+
+    public void configure(HttpSecurity httpSecurity) throws Exception{
+        JwtTokenFilter customFilter = new JwtTokenFilter(jwtTokenProdvider);
+        httpSecurity.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
