7c03cd67f9019fdf41db7330904ccfbb3f0ebbe1
hyunin <hyunin0102@gmail.com>
2022-01-24 21:49:47 +0900

    (UserRepository) findByEmail, findByName method create

---
+++ src/main/java/com/server/Puzzle/domain/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.server.Puzzle.domain.user.repository;
+
+import com.server.Puzzle.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByName(String username);
+}
