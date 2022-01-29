958c0718d94e8cb12cb0691b44b3391562acd6af
hyunin <hyunin0102@gmail.com>
2022-01-29 22:15:54 +0900

    (OauthServiceImpl) OauthService 구현체 생성

--- src/main/java/com/server/Puzzle/domain/oauth2/service/impl/OauthServiceImpl.java
+++ src/main/java/com/server/Puzzle/domain/oauth2/service/impl/OauthServiceImpl.java
@@ -1,11 +1,13 @@
-package com.server.Puzzle.domain.user.oauth2.service;
+package com.server.Puzzle.domain.oauth2.service.impl;
 
+import com.server.Puzzle.domain.oauth2.service.OauthService;
 import com.server.Puzzle.domain.user.domain.User;
-import com.server.Puzzle.domain.user.enumType.OauthAttributes;
-import com.server.Puzzle.domain.user.oauth2.OauthProperties;
-import com.server.Puzzle.domain.user.oauth2.dto.LoginResponse;
-import com.server.Puzzle.domain.user.oauth2.dto.OauthTokenResponse;
-import com.server.Puzzle.domain.user.oauth2.dto.UserProfile;
+import com.server.Puzzle.domain.oauth2.config.OauthAttributes;
+import com.server.Puzzle.domain.oauth2.config.OauthProperties;
+import com.server.Puzzle.domain.oauth2.dto.LoginResponse;
+import com.server.Puzzle.domain.oauth2.dto.OauthCode;
+import com.server.Puzzle.domain.oauth2.dto.OauthTokenResponse;
+import com.server.Puzzle.domain.oauth2.dto.UserProfile;
 import com.server.Puzzle.domain.user.repository.UserRepository;
 import com.server.Puzzle.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
@@ -22,15 +24,18 @@ import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
-public class OauthService {
+public class OauthServiceImpl implements OauthService {
     private final UserRepository userRepository;
     private final OauthProperties oauthProperties;
     private final JwtTokenProvider jwtTokenProvider;
-    public LoginResponse login(String providerName, String code) {
+    private final OauthAttributes oauthAttributes;
+
+    @Override
+    public LoginResponse login(OauthCode code) {
         // access token 가져오기
         OauthTokenResponse tokenResponse = getToken(code);
         // 유저 정보 가져오기
-        UserProfile userProfile = getUserProfile(providerName, tokenResponse);
+        UserProfile userProfile = getUserProfile(tokenResponse);
         // 유저 DB에 저장
         User user = saveOrUpdate(userProfile);
 
@@ -38,16 +43,10 @@ public class OauthService {
         String refreshToken = jwtTokenProvider.createRefreshToken();
 
         return LoginResponse.builder()
-                .id(user.getId())
-                .oauthId(user.getOauthId())
                 .name(user.getName())
                 .email(user.getEmail())
-                .imageUrl(user.getImageUrl())
-                .tokenType("Bearer")
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
-                .roles(user.getRoles())
-                .bio(user.getBio())
                 .build();
     }
 
@@ -56,12 +55,13 @@ public class OauthService {
                 .map(entity -> entity.update(
                         userProfile.getEmail(), userProfile.getName()))
                 .orElseGet(userProfile::toUser);
+        System.out.println(user);
         return userRepository.save(user);
     }
 
-    private UserProfile getUserProfile(String providerName, OauthTokenResponse tokenResponse) {
+    private UserProfile getUserProfile(OauthTokenResponse tokenResponse) {
         Map<String, Object> userAttributes = getUserAttributes(tokenResponse);
-        return OauthAttributes.extract(providerName, userAttributes);
+        return oauthAttributes.extract(userAttributes);
     }
 
     private Map<String, Object> getUserAttributes(OauthTokenResponse tokenResponse) {
@@ -74,7 +74,7 @@ public class OauthService {
                 .block();
     }
 
-    private OauthTokenResponse getToken(String code) {
+    private OauthTokenResponse getToken(OauthCode code) {
         return WebClient.create()
                 .post()
                 .uri(oauthProperties.getTokenUri())
@@ -90,9 +90,9 @@ public class OauthService {
                 .block();
     }
 
-    private MultiValueMap<String, String> tokenRequest(String code) {
+    private MultiValueMap<String, String> tokenRequest(OauthCode code) {
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
-        formData.add("code", code);
+        formData.add("code", code.getCode());
         formData.add("grant_type", "authorization_code");
         formData.add("redirect_uri", oauthProperties.getRedirectUri());
         return formData;
