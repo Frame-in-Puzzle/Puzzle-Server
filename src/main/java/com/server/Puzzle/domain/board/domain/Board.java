2f422664fd9fdc0b1ea6fe7bf610bd8bd671ab2b
노경준 <developerjun0615@gmail.com>
2022-01-30 00:04:47 +0900

    [Update] Board 엔티티 생성자, 제약조건 추가

--- src/main/java/com/server/Puzzle/domain/board/domain/Board.java
+++ src/main/java/com/server/Puzzle/domain/board/domain/Board.java
@@ -4,9 +4,16 @@ import com.server.Puzzle.domain.board.enumType.Purpose;
 import com.server.Puzzle.domain.board.enumType.Status;
 import com.server.Puzzle.domain.user.domain.User;
 import com.server.Puzzle.global.entity.BaseTimeEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder @Getter
 @Entity @Table(name = "Board")
 public class Board extends BaseTimeEntity {
 
@@ -14,20 +21,22 @@ public class Board extends BaseTimeEntity {
     @Column(name = "board_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
     @Column(name = "board_title", nullable = false)
     private String title;
 
     @Column(name = "board_contents", nullable = false)
     private String contents;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "board_purpose", nullable = false)
     private Purpose purpose;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "board_status", nullable = false)
     private Status status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
 }
