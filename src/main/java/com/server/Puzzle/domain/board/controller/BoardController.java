9a139ccac8ccc63c9f8d9a8ef0e8819006537712
노경준 <developerjun0615@gmail.com>
2022-01-30 00:12:40 +0900

    [Update] BoardController 게시물 등록 기능 추가

--- src/main/java/com/server/Puzzle/domain/board/controller/BoardController.java
+++ src/main/java/com/server/Puzzle/domain/board/controller/BoardController.java
@@ -1,4 +1,26 @@
 package com.server.Puzzle.domain.board.controller;
 
+import com.server.Puzzle.domain.board.dto.request.PostRequestDto;
+import com.server.Puzzle.domain.board.service.BoardService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/v1/api/board")
+@RestController
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
+    @ResponseStatus( HttpStatus.OK )
+    @PostMapping("/")
+    public void post(@RequestBody PostRequestDto request){
+        boardService.post(request);
+    }
 }
