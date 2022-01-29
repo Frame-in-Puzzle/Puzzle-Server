eba7e88fae46c78d1b41283e3865ddc6ea818fb1
hyunin <hyunin0102@gmail.com>
2022-01-29 22:15:31 +0900

    (OauthService) OauthService Interface 생성

---
+++ src/main/java/com/server/Puzzle/domain/oauth2/service/OauthService.java
@@ -0,0 +1,7 @@
+package com.server.Puzzle.domain.oauth2.service;
+
+import com.server.Puzzle.domain.oauth2.dto.LoginResponse;
+import com.server.Puzzle.domain.oauth2.dto.OauthCode;
+public interface OauthService {
+    public LoginResponse login(OauthCode code);
+}
