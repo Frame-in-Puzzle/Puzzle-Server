c0356d2c7e4df928ed8e452f724368810ede7da8
hyunin <hyunin0102@gmail.com>
2022-01-31 18:58:59 +0900

    (UserServiceTest) 로그인 한 유저를 확인하는 테스트 코드

---
+++ src/test/java/com/server/Puzzle/service/user/UserServiceTest.java
@@ -0,0 +1,61 @@
+package com.server.Puzzle.service.user;
+
+import com.server.Puzzle.domain.user.domain.User;
+import com.server.Puzzle.global.enumType.Field;
+import com.server.Puzzle.domain.user.repository.UserRepository;
+import com.server.Puzzle.global.enumType.Role;
+import com.server.Puzzle.global.util.CurrentUserUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+public class UserServiceTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @Autowired
+    CurrentUserUtil currentUserUtil;
+    @DisplayName("로그인 한 유저를 확인하는 테스트")
+    @Test
+    void getCurrentUserTest() throws IOException {
+        //given
+        User user = User.builder()
+                .oauthId("68847615")
+                .email("hyunin0102@gmail.com")
+                .name("홍현인")
+                .imageUrl("https://avatars.githubusercontent.com/u/68847615?v=4")
+                .bio("한줄소개")
+                .field(Field.BACKEND)
+                .roles(List.of(Role.USER))
+                .url("https://github.com/honghyunin")
+                .isFirstVisit(true)
+                .build();
+
+        userRepository.save(user);
+
+        //when
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getName(), "password", user.getRoles());
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(token);
+
+        User currentUser = currentUserUtil.getCurrentUser();
+
+        // then
+        assertEquals("홍현인", currentUser.getName());
+        assertEquals("hyunin0102@gmail.com", currentUser.getEmail());
+    }
+}
