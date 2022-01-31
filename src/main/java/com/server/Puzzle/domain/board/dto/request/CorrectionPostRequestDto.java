589455efd2b68acf14bb0aac06d52f5d5628ad29
노경준 <developerjun0615@gmail.com>
2022-01-31 18:11:39 +0900

    [ADD] CorrectionPostRequestDto 게시물 수정 기능 개발

---
+++ src/main/java/com/server/Puzzle/domain/board/dto/request/CorrectionPostRequestDto.java
@@ -0,0 +1,21 @@
+package com.server.Puzzle.domain.board.dto.request;
+
+import com.server.Puzzle.domain.board.enumType.Purpose;
+import com.server.Puzzle.domain.board.enumType.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class CorrectionPostRequestDto {
+
+    private String title;
+    private String contents;
+    private Purpose purpose;
+    private Status status;
+
+}
