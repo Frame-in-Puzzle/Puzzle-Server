fe61835625e40cb0941596fad7770ae66af944da
노경준 <developerjun0615@gmail.com>
2022-01-31 18:12:01 +0900

    [Update] PostRequestDto 게시물 등록 테스트 코드를 짜기위한 수정

--- src/main/java/com/server/Puzzle/domain/board/dto/request/PostRequestDto.java
+++ src/main/java/com/server/Puzzle/domain/board/dto/request/PostRequestDto.java
@@ -2,8 +2,14 @@ package com.server.Puzzle.domain.board.dto.request;
 
 import com.server.Puzzle.domain.board.enumType.Purpose;
 import com.server.Puzzle.domain.board.enumType.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 public class PostRequestDto {
 
