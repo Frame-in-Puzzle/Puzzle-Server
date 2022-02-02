0f033b1ea99ac7289769082f81a75b7ec2c970c4
노경준 <developerjun0615@gmail.com>
2022-02-02 19:43:53 +0900

    [ADD] BoardFile 추가

---
+++ src/main/java/com/server/Puzzle/domain/board/domain/BoardFile.java
@@ -0,0 +1,26 @@
+package com.server.Puzzle.domain.board.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder @Getter
+@Entity @Table(name = "File")
+public class BoardFile {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "file_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @Column(name = "file_filename")
+    private String filename;
+}
