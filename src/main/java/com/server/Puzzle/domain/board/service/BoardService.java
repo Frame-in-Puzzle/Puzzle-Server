14081c23c3dafb7dfdd27e3b7def2b784cb33b41
노경준 <developerjun0615@gmail.com>
2022-02-08 12:32:51 +0900

    [Update] BoardService create-url 기능 ( 이미지 업로드 기능 추가 )

--- src/main/java/com/server/Puzzle/domain/board/service/BoardService.java
+++ src/main/java/com/server/Puzzle/domain/board/service/BoardService.java
@@ -5,9 +5,8 @@ import com.server.Puzzle.domain.board.dto.request.CorrectionPostRequestDto;
 import com.server.Puzzle.domain.board.dto.request.PostRequestDto;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
 public interface BoardService {
-    Board post(List<MultipartFile> files, PostRequestDto request);
+    Board post(PostRequestDto request);
+    String createUrl(MultipartFile files);
     Board correctionPost(Long id, CorrectionPostRequestDto request);
 }
