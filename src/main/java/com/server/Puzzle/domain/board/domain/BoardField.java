dd3512e4c9e3f3f396e5f029d3f47885c40b38c6
노경준 <developerjun0615@gmail.com>
2022-02-10 02:43:55 +0900

    [Update] BoardField 게시물 작성 에서 boardFields 와 boardLanguages 를 저장할 수 있도록 수정

--- src/main/java/com/server/Puzzle/domain/board/domain/BoardField.java
+++ src/main/java/com/server/Puzzle/domain/board/domain/BoardField.java
@@ -3,9 +3,16 @@ package com.server.Puzzle.domain.board.domain;
 
 import com.server.Puzzle.global.entity.BaseTimeEntity;
 import com.server.Puzzle.global.enumType.Field;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder @Getter
 @Entity @Table(name = "Board_Field")
 public class BoardField extends BaseTimeEntity {
 
@@ -17,6 +24,7 @@ public class BoardField extends BaseTimeEntity {
     @JoinColumn(name = "board_id", nullable = false)
     private Board board;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "field", nullable = false)
     private Field field;
 
