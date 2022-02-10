ae7e8ff2f7bc18f6e6dab13052f819cb9a8b37a3
노경준 <developerjun0615@gmail.com>
2022-02-10 13:28:26 +0900

    [Update] BoardLanguageRepository 게시물 수정 에서 boardFields 와 boardLanguages 를 수정할 수 있도록 수정

--- src/main/java/com/server/Puzzle/domain/board/repository/BoardLanguageRepository.java
+++ src/main/java/com/server/Puzzle/domain/board/repository/BoardLanguageRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardLanguageRepository extends JpaRepository<BoardLanguage, Long> {
+    void deleteByBoardId(Long id);
 }
