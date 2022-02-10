b317e34665c476f23578e77505acad90289ec9be
노경준 <developerjun0615@gmail.com>
2022-02-10 22:47:42 +0900

    [Update] BoardController 게시물 전체조회 기능 개발

--- src/main/java/com/server/Puzzle/domain/board/controller/BoardController.java
+++ src/main/java/com/server/Puzzle/domain/board/controller/BoardController.java
@@ -2,10 +2,14 @@ package com.server.Puzzle.domain.board.controller;
 
 import com.server.Puzzle.domain.board.dto.request.CorrectionPostRequestDto;
 import com.server.Puzzle.domain.board.dto.request.PostRequestDto;
+import com.server.Puzzle.domain.board.dto.response.GetAllResponseDto;
 import com.server.Puzzle.domain.board.service.BoardService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -42,6 +46,15 @@ public class BoardController {
     @PutMapping("/{id}")
     public void correctionPost(@PathVariable("id") Long id, @RequestBody CorrectionPostRequestDto request){
         boardService.correctionPost(id, request);
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = false, dataType = "String", paramType = "header")
+    })
+    @ResponseStatus( HttpStatus.OK )
+    @GetMapping
+    public Page<GetAllResponseDto> getAllPost(@PageableDefault(size = 12) Pageable pageable) {
+        return boardService.getAllPost(pageable);
     }
 
 }
