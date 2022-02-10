63b760ebb06d39ca7d62937f731d74528e6ba25c
노경준 <developerjun0615@gmail.com>
2022-02-11 08:53:57 +0900

    [Update] BoardService 게시물을 단일 조회하는 기능 개발

--- src/main/java/com/server/Puzzle/domain/board/service/BoardService.java
+++ src/main/java/com/server/Puzzle/domain/board/service/BoardService.java
@@ -4,6 +4,7 @@ import com.server.Puzzle.domain.board.domain.Board;
 import com.server.Puzzle.domain.board.dto.request.CorrectionPostRequestDto;
 import com.server.Puzzle.domain.board.dto.request.PostRequestDto;
 import com.server.Puzzle.domain.board.dto.response.GetAllResponseDto;
+import com.server.Puzzle.domain.board.dto.response.GetResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,4 +14,5 @@ public interface BoardService {
     String createUrl(MultipartFile files);
     Board correctionPost(Long id, CorrectionPostRequestDto request);
     Page<GetAllResponseDto> getAllPost(Pageable pageable);
+    GetResponseDto getPost(Long id);
 }
