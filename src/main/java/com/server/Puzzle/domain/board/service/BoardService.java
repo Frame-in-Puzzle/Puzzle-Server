f04322b497410285b207e6044146a89b5dcb37cb
노경준 <developerjun0615@gmail.com>
2022-02-10 22:48:58 +0900

    [Update] BoardService 게시물 전체조회 기능 개발

--- src/main/java/com/server/Puzzle/domain/board/service/BoardService.java
+++ src/main/java/com/server/Puzzle/domain/board/service/BoardService.java
@@ -3,10 +3,14 @@ package com.server.Puzzle.domain.board.service;
 import com.server.Puzzle.domain.board.domain.Board;
 import com.server.Puzzle.domain.board.dto.request.CorrectionPostRequestDto;
 import com.server.Puzzle.domain.board.dto.request.PostRequestDto;
+import com.server.Puzzle.domain.board.dto.response.GetAllResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface BoardService {
     Board post(PostRequestDto request);
     String createUrl(MultipartFile files);
     Board correctionPost(Long id, CorrectionPostRequestDto request);
+    Page<GetAllResponseDto> getAllPost(Pageable pageable);
 }
