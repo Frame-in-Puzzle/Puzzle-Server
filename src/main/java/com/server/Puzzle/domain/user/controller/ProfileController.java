20f6feaafe383cde6b95b47d74eab2f7a940d4ce
hyunin <hyunin0102@gmail.com>
2022-02-11 16:15:34 +0900

    (BoardRepository) add findBoardsByUser method

--- src/main/java/com/server/Puzzle/domain/user/controller/ProfileController.java
+++ src/main/java/com/server/Puzzle/domain/user/controller/ProfileController.java
@@ -1,10 +1,14 @@
 package com.server.Puzzle.domain.user.controller;
 
+import com.server.Puzzle.domain.user.dto.MyBoardResponse;
 import com.server.Puzzle.domain.user.dto.UserInfoDto;
 import com.server.Puzzle.domain.user.service.ProfileService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,9 +22,9 @@ public class ProfileController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 토큰", required = true, dataType = "String", paramType = "header")
     })
-    @GetMapping("/{name}")
-    public ResponseEntity<UserInfoDto> getProfile(@PathVariable String name) {
-        UserInfoDto userInfo = profileService.getProfile(name);
+    @GetMapping("/{githubId}")
+    public ResponseEntity<UserInfoDto> getProfile(@PathVariable String githubId) {
+        UserInfoDto userInfo = profileService.getProfile(githubId);
         return ResponseEntity.ok().body(userInfo);
     }
 
@@ -30,5 +34,13 @@ public class ProfileController {
     @PutMapping("/update")
     public void profileUpdate(@RequestBody UserInfoDto userInfo) {
         profileService.profileUpdate(userInfo);
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 토큰", required = true, dataType = "String", paramType = "header")
+    })
+    @GetMapping("/board/{githubId}")
+    public Page<MyBoardResponse> getMyBoard(@PageableDefault(page = 10) Pageable pageable, @PathVariable String githubId) {
+        return profileService.getMyBoard(githubId, pageable);
     }
 }
