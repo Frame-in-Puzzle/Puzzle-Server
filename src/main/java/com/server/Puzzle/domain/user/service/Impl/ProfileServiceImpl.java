f2dc0101a045ea8e4077491f26dc281fbfac3da9
hyunin <hyunin0102@gmail.com>
2022-02-11 16:16:36 +0900

    (ProfileServiceImpl) add getMyBoard method

--- src/main/java/com/server/Puzzle/domain/user/service/Impl/ProfileServiceImpl.java
+++ src/main/java/com/server/Puzzle/domain/user/service/Impl/ProfileServiceImpl.java
@@ -1,5 +1,7 @@
 package com.server.Puzzle.domain.user.service.Impl;
 
+import com.server.Puzzle.domain.board.domain.BoardFile;
+import com.server.Puzzle.domain.board.repository.BoardRepository;
 import com.server.Puzzle.domain.user.domain.User;
 import com.server.Puzzle.domain.user.dto.MyBoardResponse;
 import com.server.Puzzle.domain.user.dto.UserInfoDto;
@@ -7,20 +9,22 @@ import com.server.Puzzle.domain.user.repository.UserRepository;
 import com.server.Puzzle.domain.user.service.ProfileService;
 import com.server.Puzzle.global.util.CurrentUserUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ProfileServiceImpl implements ProfileService {
     private final CurrentUserUtil currentUserUtil;
     private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
 
     @Override
-    public UserInfoDto getProfile(String name) {
-        User user = userRepository.findByName(name)
+    public UserInfoDto getProfile(String githubId) {
+        User user = userRepository.findByGithubId(githubId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다"));
 
         UserInfoDto userInfo = new UserInfoDto(user);
@@ -46,10 +50,22 @@ public class ProfileServiceImpl implements ProfileService {
     }
 
     @Override
-    public List<MyBoardResponse> getMyBoard() {
-        String user = currentUserUtil.getCurrentUser().getName();
-
-        return null;
+    public Page<MyBoardResponse> getMyBoard(String githubId, Pageable pageable) {
+        User user = userRepository.findByGithubId(githubId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다"));
+        return boardRepository.findBoardsByUser(user, pageable)
+                .map(
+                board -> MyBoardResponse.builder()
+                        .title(board.getTitle())
+                        .contents(board.getContents())
+                        .date(board.getCreatedDate())
+                        .status(board.getStatus())
+                        .files(
+                                board.getBoardFiles().stream()
+                                .map(BoardFile::getUrl)
+                                        .findFirst()
+                                        .orElse(null))
+                        .build());
     }
 
 }
