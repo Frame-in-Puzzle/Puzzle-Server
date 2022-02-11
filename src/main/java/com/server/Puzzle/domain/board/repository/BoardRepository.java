860a712d5db49eff3279ce170688422758704d58
hyunin <hyunin0102@gmail.com>
2022-02-11 16:15:07 +0900

    (BoardRepository) add findBoardsByUser method

--- src/main/java/com/server/Puzzle/domain/board/repository/BoardRepository.java
+++ src/main/java/com/server/Puzzle/domain/board/repository/BoardRepository.java
@@ -1,9 +1,13 @@
 package com.server.Puzzle.domain.board.repository;
 
 import com.server.Puzzle.domain.board.domain.Board;
+import com.server.Puzzle.domain.user.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardRepository extends JpaRepository<Board,Long>, BoardCustomRepository {
+public interface BoardRepository extends JpaRepository<Board,Long> {
+    Page<Board> findBoardsByUser(User user, Pageable pageable);
 }
