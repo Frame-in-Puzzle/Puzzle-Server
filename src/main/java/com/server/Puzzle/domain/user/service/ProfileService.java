8804ca8a15f80c35ba087eee279c542df2a261d0
hyunin <hyunin0102@gmail.com>
2022-02-11 16:15:49 +0900

    (ProfileService) add getMyBoard method

--- src/main/java/com/server/Puzzle/domain/user/service/ProfileService.java
+++ src/main/java/com/server/Puzzle/domain/user/service/ProfileService.java
@@ -2,11 +2,11 @@ package com.server.Puzzle.domain.user.service;
 
 import com.server.Puzzle.domain.user.dto.MyBoardResponse;
 import com.server.Puzzle.domain.user.dto.UserInfoDto;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProfileService {
     UserInfoDto getProfile(String name);
     void profileUpdate(UserInfoDto userInfo);
-    List<MyBoardResponse> getMyBoard();
+    Page<MyBoardResponse> getMyBoard(String githubId, Pageable pageable);
 }
