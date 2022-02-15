a75770da75653ff4d8279b581c9eea63aee3345d
노경준 <developerjun0615@gmail.com>
2022-02-15 18:45:04 +0900

    [ADD] BoardCustomRepository 게시물을 태그로 조회하는 기능 추가

---
+++ src/main/java/com/server/Puzzle/domain/board/repository/BoardCustomRepository.java
@@ -0,0 +1,16 @@
+package com.server.Puzzle.domain.board.repository;
+
+import com.server.Puzzle.domain.board.dto.response.GetPostByTagResponseDto;
+import com.server.Puzzle.domain.board.enumType.Purpose;
+import com.server.Puzzle.domain.board.enumType.Status;
+import com.server.Puzzle.global.enumType.Field;
+import com.server.Puzzle.global.enumType.Language;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface BoardCustomRepository {
+
+    List<GetPostByTagResponseDto> findBoardByTag(Purpose purpose, List<Field> field, List<Language> language, Status status, Pageable pageable);
+
+}
