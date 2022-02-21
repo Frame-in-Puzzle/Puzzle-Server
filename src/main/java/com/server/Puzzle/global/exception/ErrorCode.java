c7577404aaa1033ae9cd22c0e5f18985c295abfa
hyunin <hyunin0102@gmail.com>
2022-02-21 21:24:03 +0900

    [ADD] PARAMETER_IS_MISSING 추가

--- src/main/java/com/server/Puzzle/global/exception/ErrorCode.java
+++ src/main/java/com/server/Puzzle/global/exception/ErrorCode.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.HttpStatus.*;
 
 
 @Getter
@@ -19,7 +18,10 @@ public enum ErrorCode {
     /* 401 UNAUTHORIZED : 인증되지 않은 사용자 */
     UNAUTHORIZED_USER(UNAUTHORIZED, "인증되지 않은 사용자입니다"),
     BOARD_NOT_HAVE_PERMISSION_TO_DELETE(UNAUTHORIZED, "해당 게시글을 삭제할 권한이 없습니다"),
-    BOARD_NOT_HAVE_PERMISSION_TO_MODIFY(UNAUTHORIZED, "해당 게시글을 수정할 권한이 없습니다");
+    BOARD_NOT_HAVE_PERMISSION_TO_MODIFY(UNAUTHORIZED, "해당 게시글을 수정할 권한이 없습니다"),
+
+    /* 400 BAD_REQUEST : 잘못된 요청 */
+    PARAMETER_IS_MISSING(BAD_REQUEST, "잘못된 경로로 접근하였습니다");
 
     private final HttpStatus httpStatus;
     private final String detail;
