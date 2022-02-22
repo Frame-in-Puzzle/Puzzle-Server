b5ffe95ccef30660ae70950707c0383e42f1e14e
hyunin <hyunin0102@gmail.com>
2022-02-23 00:42:44 +0900

    [UPDATE] Response 방식 변경

--- src/main/java/com/server/Puzzle/domain/user/controller/ProfileController.java
+++ src/main/java/com/server/Puzzle/domain/user/controller/ProfileController.java
@@ -4,10 +4,6 @@ import com.server.Puzzle.domain.user.dto.MyBoardResponse;
 import com.server.Puzzle.domain.user.dto.UserInfoDto;
 import com.server.Puzzle.domain.user.service.ProfileService;
 import com.server.Puzzle.global.exception.CustomException;
-import com.server.Puzzle.global.exception.ErrorCode;
-import com.server.Puzzle.global.response.ResponseService;
-import com.server.Puzzle.global.response.result.CommonResult;
-import com.server.Puzzle.global.response.result.SingleResult;
 import com.server.Puzzle.global.util.CurrentUserUtil;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -15,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
@@ -28,33 +25,32 @@ public class ProfileController {
 
     private final CurrentUserUtil currentUserUtil;
     private final ProfileService profileService;
-    private final ResponseService responseService;
 
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 토큰", required = true, dataType = "String", paramType = "header")
     })
     @GetMapping("/{githubId}")
-    public SingleResult<UserInfoDto> getProfile(@PathVariable String githubId) {
+    public ResponseEntity<UserInfoDto> getProfile(@PathVariable String githubId) {
         UserInfoDto userInfo = profileService.getProfile(githubId);
-        return responseService.getSingleResult(userInfo);
+        return ResponseEntity.ok().body(userInfo);
     }
 
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 토큰", required = true, dataType = "String", paramType = "header")
     })
     @PutMapping("/update")
-    public CommonResult profileUpdate(@RequestBody UserInfoDto userInfo) {
+    public ResponseEntity profileUpdate(@RequestBody UserInfoDto userInfo) {
         profileService.profileUpdate(userInfo);
-        return responseService.getSuccessResult();
+        return ResponseEntity.ok().build();
     }
 
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 토큰", required = true, dataType = "String", paramType = "header")
     })
     @GetMapping("/board/{githubId}")
-    public SingleResult<Page<MyBoardResponse>> getMyBoard(@PageableDefault(page = 10) Pageable pageable, @PathVariable String githubId) {
+    public ResponseEntity<Page<MyBoardResponse>> getMyBoard(@PageableDefault(page = 10) Pageable pageable, @PathVariable String githubId) {
         if(!Objects.equals(currentUserUtil.getCurrentUser().getGithubId(), githubId))
             throw new CustomException(UNAUTHORIZED_USER);
-        return responseService.getSingleResult(profileService.getMyBoard(githubId, pageable));
+        return ResponseEntity.ok().body(profileService.getMyBoard(githubId, pageable));
     }
 }
