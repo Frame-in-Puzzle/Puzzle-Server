6205a4ad599e25f303884e563b871127ba23519f
hyunin <hyunin0102@gmail.com>
2022-02-27 01:48:34 +0900

    [ADD] UserUpdateDto

---
+++ src/main/java/com/server/Puzzle/domain/user/dto/UserUpdateDto.java
@@ -0,0 +1,18 @@
+package com.server.Puzzle.domain.user.dto;
+
+import com.server.Puzzle.global.enumType.Field;
+import com.server.Puzzle.global.enumType.Language;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class UserUpdateDto {
+    private String name;
+    private String email;
+    private String imageUrl;
+    private String bio;
+    private Field field;
+    private List<Language> language;
+    private String url;
+}
