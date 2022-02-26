e744d89da06f02492bcb1998584a4fa52e4d537e
노경준 <developerjun0615@gmail.com>
2022-02-26 23:58:03 +0900

    [Update] Attend Entity 수정

--- src/main/java/com/server/Puzzle/domain/attend/domain/Attend.java
+++ src/main/java/com/server/Puzzle/domain/attend/domain/Attend.java
@@ -1,10 +1,12 @@
 package com.server.Puzzle.domain.attend.domain;
 
 import com.server.Puzzle.domain.board.domain.Board;
+import com.server.Puzzle.domain.user.domain.User;
 import com.server.Puzzle.global.entity.BaseTimeEntity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -20,6 +22,14 @@ public class Attend extends BaseTimeEntity {
     @JoinColumn(name = "board_id", nullable = false)
     private Board board;
 
-    private String githubId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
+    @OneToMany(
+            mappedBy = "attend",
+            cascade = CascadeType.REMOVE,
+            orphanRemoval = true
+    )
+    private List<AttendLanguage> languages;
 }
