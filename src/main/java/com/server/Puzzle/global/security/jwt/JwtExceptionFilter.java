9ca760db3cc84d30507cb5ffcb626fad87a315ea
hyunin <hyunin0102@gmail.com>
2022-02-27 03:22:41 +0900

    [ADD] 예외처리 추가, 에러 메시지 로깅

--- src/main/java/com/server/Puzzle/global/security/jwt/JwtExceptionFilter.java
+++ src/main/java/com/server/Puzzle/global/security/jwt/JwtExceptionFilter.java
@@ -3,20 +3,22 @@ package com.server.Puzzle.global.security.jwt;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.server.Puzzle.global.exception.ErrorCode;
 import com.server.Puzzle.global.exception.ErrorResponse;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.entity.ContentType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import static com.server.Puzzle.global.exception.ErrorCode.*;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class JwtExceptionFilter extends OncePerRequestFilter {
@@ -27,8 +29,13 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException {
         try {
             chain.doFilter(req, res);
-        } catch (JwtException | ServletException ex) {
+        } catch (ExpiredJwtException ex) {
             setErrorResponse(EXPIRED_TOKEN, res);
+        } catch (JwtException ex) {
+            setErrorResponse(INVALID_TOKEN, res);
+        } catch (Exception e) {
+            log.error("============= 알 수 없는 에러 발생 =============", e);
+            setErrorResponse(UNKNOWN_ERROR, res);
         }
     }
 
