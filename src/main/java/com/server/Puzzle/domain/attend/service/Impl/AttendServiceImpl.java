422486ac19b2e9eb4b8ed4757c20959e97d1b129
노경준 <developerjun0615@gmail.com>
2022-02-27 23:52:06 +0900

    [Update] AttendServiceImpl 프로젝트 참가 요청 전체 조회를 하는 기능 구현

--- src/main/java/com/server/Puzzle/domain/attend/service/Impl/AttendServiceImpl.java
+++ src/main/java/com/server/Puzzle/domain/attend/service/Impl/AttendServiceImpl.java
@@ -1,21 +1,29 @@
 package com.server.Puzzle.domain.attend.service.Impl;
 
 import com.server.Puzzle.domain.attend.domain.Attend;
+import com.server.Puzzle.domain.attend.domain.AttendLanguage;
+import com.server.Puzzle.domain.attend.dto.response.GetAllAttendResponse;
+import com.server.Puzzle.domain.attend.repository.AttendLanguageRepository;
 import com.server.Puzzle.domain.attend.repository.AttendRepository;
 import com.server.Puzzle.domain.attend.service.AttendService;
 import com.server.Puzzle.domain.board.domain.Board;
 import com.server.Puzzle.domain.board.repository.BoardRepository;
+import com.server.Puzzle.domain.user.domain.User;
+import com.server.Puzzle.domain.user.domain.UserLanguage;
 import com.server.Puzzle.global.exception.CustomException;
 import com.server.Puzzle.global.exception.ErrorCode;
 import com.server.Puzzle.global.util.CurrentUserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class AttendServiceImpl implements AttendService {
 
     private final AttendRepository attendRepository;
+    private final AttendLanguageRepository attendLanguageRepository;
     private final BoardRepository boardRepository;
     private final CurrentUserUtil currentUserUtil;
 
@@ -24,15 +32,33 @@ public class AttendServiceImpl implements AttendService {
         Board board = boardRepository.findById(boardId)
                 .orElseThrow(() -> new CustomException(ErrorCode.BOARD_NOT_FOUND));
 
-        String githubId = currentUserUtil.getCurrentUser().getGithubId();
+        User currentUser = currentUserUtil.getCurrentUser();
 
-        if(board.isAttended(githubId)) throw new CustomException(ErrorCode.IS_ALREADY_ATTEND);
+        if(board.isAttended(currentUser)) throw new CustomException(ErrorCode.IS_ALREADY_ATTEND);
 
-        attendRepository.save(
+        Attend attend = attendRepository.save(
                 Attend.builder()
-                    .board(board)
-                    .githubId(githubId)
-                    .build()
+                        .board(board)
+                        .user(currentUser)
+                        .build()
         );
+
+        for (UserLanguage userLanguage : currentUser.getUserLanguages()) {
+            attendLanguageRepository.save(
+                    AttendLanguage.builder()
+                            .attend(attend)
+                            .language(userLanguage.getLanguage())
+                            .build()
+            );
+        }
+
+    }
+
+    @Override
+    public List<GetAllAttendResponse> findAllAttend(Long boardId) {
+        boardRepository.findById(boardId)
+                .orElseThrow(() -> new CustomException(ErrorCode.BOARD_NOT_FOUND));
+
+        return attendRepository.findAllByBoardId(boardId);
     }
 }
