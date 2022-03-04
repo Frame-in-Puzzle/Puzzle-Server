f88597ecf3057373f44976a46d67914c4b5d2853
노경준 <developerjun0615@gmail.com>
2022-03-04 09:13:04 +0900

    [Update] BoardServiceTest board post 기능 테스트 코드 작성

--- src/test/java/com/server/Puzzle/service/board/BoardServiceTest.java
+++ src/test/java/com/server/Puzzle/service/board/BoardServiceTest.java
@@ -1,14 +1,15 @@
 package com.server.Puzzle.service.board;
 
 import com.server.Puzzle.domain.board.domain.Board;
-import com.server.Puzzle.domain.board.dto.request.CorrectionPostRequestDto;
 import com.server.Puzzle.domain.board.dto.request.PostRequestDto;
 import com.server.Puzzle.domain.board.enumType.Purpose;
 import com.server.Puzzle.domain.board.enumType.Status;
+import com.server.Puzzle.domain.board.repository.BoardRepository;
 import com.server.Puzzle.domain.board.service.BoardService;
 import com.server.Puzzle.domain.user.domain.User;
 import com.server.Puzzle.domain.user.repository.UserRepository;
 import com.server.Puzzle.global.enumType.Field;
+import com.server.Puzzle.global.enumType.Language;
 import com.server.Puzzle.global.enumType.Role;
 import com.server.Puzzle.global.util.CurrentUserUtil;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,16 +24,20 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import javax.transaction.Transactional;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Transactional
 @SpringBootTest
 public class BoardServiceTest {
 
-    @Autowired private BoardService boardService;
-    @Autowired private UserRepository userRepository;
-    @Autowired private CurrentUserUtil currentUserUtil;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BoardRepository boardRepository;
+    @Autowired
+    private BoardService boardService;
+    @Autowired
+    private CurrentUserUtil currentUserUtil;
 
     @BeforeEach
     @DisplayName("로그인 되어있는 유저를 확인하는 테스트")
@@ -41,6 +46,7 @@ public class BoardServiceTest {
         User user = User.builder()
                 .oauthIdx("1234")
                 .email("developerjun0615@gmail.com")
+                .githubId("KyungJunNoh2")
                 .name("노경준")
                 .field(Field.BACKEND)
                 .roles(List.of(Role.USER))
@@ -60,47 +66,29 @@ public class BoardServiceTest {
         User currentUser = currentUserUtil.getCurrentUser();
 
         // then
-        assertEquals(user.getEmail(),currentUser.getEmail());
+        assertNotNull(currentUser);
     }
 
-//    @Test
-//    @DisplayName("게시물을 등록 기능을 테스트하는 테스트")
-//    void postTest(){
-//        PostRequestDto requestDto = PostRequestDto.builder()
-//                .title("string")
-//                .contents("contents")
-//                .purpose(Purpose.PROJECT)
-//                .status(Status.RECRUITMENT)
-//                .build();
-//
-//        Board post = boardService.post(null, requestDto);
-//
-//        assertThat(post.getTitle()).isEqualTo(requestDto.getTitle());
-//    }
+    @Test
+    @DisplayName("게시물을 등록 기능을 테스트하는 테스트")
+    void postTest(){
+        PostRequestDto postRequestDto = PostRequestDto.builder()
+                .title("title")
+                .contents("contents")
+                .purpose(Purpose.PROJECT)
+                .status(Status.RECRUITMENT)
+                .fieldList(List.of(Field.BACKEND,Field.FRONTEND))
+                .languageList(List.of(Language.JAVA,Language.TS))
+                .fileUrlList(List.of("google.com","naver.com"))
+                .build();
 
-//    @Test
-//    @DisplayName("게시물을 수정 기능을 테스트하는 테스트")
-//    void correctionPostTest(){
-//        PostRequestDto postRequestDto = PostRequestDto.builder()
-//                .title("string")
-//                .contents("contents")
-//                .purpose(Purpose.PROJECT)
-//                .status(Status.RECRUITMENT)
-//                .build();
-//
-//        Board post = boardService.post(null, postRequestDto);
-//        Long postId = post.getId();
-//
-//        CorrectionPostRequestDto correctionPostRequestDto = CorrectionPostRequestDto.builder()
-//                .title("string2")
-//                .contents("contents2")
-//                .purpose(Purpose.PROJECT)
-//                .status(Status.RECRUITMENT)
-//                .build();
-//
-//        Board correctionPost = boardService.correctionPost(postId,correctionPostRequestDto);
-//
-//        assertEquals(correctionPost.getTitle(),"string2");
-//    }
+        // when
+        boardService.post(postRequestDto);
+
+        // then
+        Board board = boardRepository.findAll().get(0);
+
+        assertNotNull(board);
+    }
+
 }
-
