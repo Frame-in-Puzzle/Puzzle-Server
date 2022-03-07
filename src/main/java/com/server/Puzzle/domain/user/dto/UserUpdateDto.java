6ffcbe3257feaa6c70aaf11cc6528d2841373abe
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-03-07 17:17:23 +0900

    [ADD] 프로필 수정 lombok 추가

--- src/main/java/com/server/Puzzle/domain/user/dto/UserUpdateDto.java
+++ src/main/java/com/server/Puzzle/domain/user/dto/UserUpdateDto.java
@@ -2,10 +2,13 @@ package com.server.Puzzle.domain.user.dto;
 
 import com.server.Puzzle.global.enumType.Field;
 import com.server.Puzzle.global.enumType.Language;
-import lombok.Getter;
+import lombok.*;
 
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 @Getter
 public class UserUpdateDto {
     private String name;
