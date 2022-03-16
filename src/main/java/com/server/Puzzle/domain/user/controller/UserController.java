64439d68cab5c3e79c9b0a68c107b87838d6e073
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-03-16 10:02:19 +0900

    [ADD] infoRegister Controller

--- src/main/java/com/server/Puzzle/domain/user/controller/UserController.java
+++ src/main/java/com/server/Puzzle/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.server.Puzzle.domain.user.controller;
 
+import com.server.Puzzle.domain.user.dto.UserUpdateDto;
 import com.server.Puzzle.domain.user.service.UserService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -31,6 +32,15 @@ public class UserController {
     @DeleteMapping("/delete")
     public ResponseEntity delete() {
         userService.delete();
+        return ResponseEntity.ok("Success");
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 토큰", required = true, dataType = "String", paramType = "header")
+    })
+    @PutMapping("/register")
+    public ResponseEntity infoRegister(@RequestBody UserUpdateDto userInfo) {
+        userService.infoRegister(userInfo);
         return ResponseEntity.ok("Success");
     }
 }
