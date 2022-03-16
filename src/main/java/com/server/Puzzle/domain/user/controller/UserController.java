4e1e02cdf8adc16a05a55666b36b2ee50e338e3b
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-03-16 11:00:28 +0900

    [DELETE] UserController v1 url

--- src/main/java/com/server/Puzzle/domain/user/controller/UserController.java
+++ src/main/java/com/server/Puzzle/domain/user/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RequestMapping("/v1/api/user")
+@RequestMapping("/api/user")
 @RestController
 public class UserController {
     private final UserService userService;
