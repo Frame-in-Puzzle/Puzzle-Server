f9d1ec35ead4fe23a5d68c797f9ec1752c8e79ec
노경준 <developerjun0615@gmail.com>
2022-03-16 10:31:03 +0900

    [Update] AttendController url 에서 /v1 제거

--- src/main/java/com/server/Puzzle/domain/attend/controller/AttendController.java
+++ src/main/java/com/server/Puzzle/domain/attend/controller/AttendController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/v1/api/attend")
+@RequestMapping("/api/attend")
 @RequiredArgsConstructor
 @RestController
 public class AttendController {
