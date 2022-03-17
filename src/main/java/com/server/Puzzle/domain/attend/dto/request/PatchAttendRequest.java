697efe1ff3f7f0f83efe2c296e5abee6506537c4
노경준 <developerjun0615@gmail.com>
2022-03-17 16:21:15 +0900

    [Update] PatchAttendRequest 테스트 코드 작성

--- src/main/java/com/server/Puzzle/domain/attend/dto/request/PatchAttendRequest.java
+++ src/main/java/com/server/Puzzle/domain/attend/dto/request/PatchAttendRequest.java
@@ -1,8 +1,12 @@
 package com.server.Puzzle.domain.attend.dto.request;
 
 import com.server.Puzzle.domain.attend.domain.AttendStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
+@AllArgsConstructor
+@Builder
 @Getter
 public class PatchAttendRequest {
 
