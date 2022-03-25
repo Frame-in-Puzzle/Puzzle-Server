6e3943233617c356d67885a8f3ed2cc15d3f595c
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-03-25 13:58:18 +0900

    [DELETE] AccessToken 제거

--- src/main/java/com/server/Puzzle/domain/user/controller/TokenController.java
+++ src/main/java/com/server/Puzzle/domain/user/controller/TokenController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiImplicitParams;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +21,7 @@ public class TokenController {
     private final UserServiceImpl userService;
     private final JwtTokenProvider jwtTokenProvider;
 
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header"),
-            @ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
+    @ApiImplicitParams({@ApiImplicitParam(name = "RefreshToken", value = "로그인 성공 후 refresh_token", required = false, dataType = "String", paramType = "header")
     })
     @GetMapping("/reissueToken")
     public ResponseEntity<Map<String, String>> reissueToken(HttpServletRequest request) {
