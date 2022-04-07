a177ec2999110c94d6e368c6db69c2c29a51ea1e
노경준 <developerjun0615@gmail.com>
2022-04-07 17:46:23 +0900

    [Update] WebConfig 프로필 설정

--- src/main/java/com/server/Puzzle/global/config/web/WebConfig.java
+++ src/main/java/com/server/Puzzle/global/config/web/WebConfig.java
@@ -1,10 +1,12 @@
 package com.server.Puzzle.global.config.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Profile({"prod","dev"})
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
