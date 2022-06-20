97a2ea6fdbbb6cca66f63e230fdd1de2418c1b4d
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-06-20 14:07:32 +0900

    [ADD] 프로필 컨트롤러 테스트 생성

---
+++ src/test/java/com/server/Puzzle/controller/profile/ProfileControllerTest.java
@@ -0,0 +1,9 @@
+package com.server.Puzzle.controller.profile;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ProfileControllerTest {
+
+}
