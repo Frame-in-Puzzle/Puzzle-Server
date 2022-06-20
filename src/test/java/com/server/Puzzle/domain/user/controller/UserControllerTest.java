45a221a71e9a5b249afc320415610e692cfce0c5
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-06-20 12:04:38 +0900

    [ADD] 첫 방문 회원정보 등록 성공 테스트 추가

--- src/test/java/com/server/Puzzle/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/server/Puzzle/domain/user/controller/UserControllerTest.java
@@ -1,18 +1,27 @@
 package com.server.Puzzle.domain.user.controller;
 
+import com.google.gson.Gson;
+import com.server.Puzzle.domain.user.dto.UserUpdateDto;
 import com.server.Puzzle.domain.user.service.UserService;
+import com.server.Puzzle.global.enumType.Field;
+import com.server.Puzzle.global.enumType.Language;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -34,9 +43,12 @@ class UserControllerTest {
     @Test
     public void 로그아웃_성공() throws Exception {
 
-        doNothing().when(userService).logout();
+        doNothing().when(userService)
+                .logout();
 
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.delete("/api/user/logout"));
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.delete("/api/user/logout")
+        );
 
         resultActions.andExpect(status().isOk());
     }
@@ -51,5 +63,33 @@ class UserControllerTest {
         );
 
         resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    public void 첫_방문_회원정보_등록_성공() throws Exception {
+        UserUpdateDto userUpdateDto = requestUserUpdateDto();
+
+        doNothing().when(userService)
+                .infoRegistration(any(UserUpdateDto.class));
+
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.put("/api/user/registration")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new Gson().toJson(userUpdateDto))
+
+        ).andDo(print());
+
+        resultActions.andExpect(status().isOk());
+    }
+
+    private UserUpdateDto requestUserUpdateDto() {
+        return UserUpdateDto.builder()
+                .name("hyunin")
+                .email("hyunin0102@gmail.com")
+                .imageUrl("https://avatars.githubusercontent.com/u/68847615?v=4")
+                .bio("i am bio")
+                .field(Field.BACKEND)
+                .languages(List.of(Language.SPRING, Language.SPRINGBOOT))
+                .build();
     }
 }
