b6c08409208809b6b2a6e7aac02253c919dccad5
hyunin <68847615+honghyunin@users.noreply.github.com>
2022-06-20 12:02:19 +0900

    [ADD] 로그아웃 성공 테스트 추가

--- src/test/java/com/server/Puzzle/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/server/Puzzle/domain/user/controller/UserControllerTest.java
@@ -1,9 +1,43 @@
 package com.server.Puzzle.domain.user.controller;
 
+import com.server.Puzzle.domain.user.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class UserControllerTest {
 
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserService userService;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    public void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+    }
+
+    @Test
+    public void 로그아웃_성공() throws Exception {
+
+        doNothing().when(userService).logout();
+
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.delete("/api/user/logout"));
+
+        resultActions.andExpect(status().isOk());
+    }
 }
