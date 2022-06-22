3ee53bf8c6b903294058fd9d06a381344f9606e6
노경준 <developerjun0615@gmail.com>
2022-06-22 12:07:18 +0900

    Mockito를 이용한 참가요청 상태확인 테스트케이스 작성

--- src/test/java/com/server/Puzzle/controller/attend/AttendControllerTest.java
+++ src/test/java/com/server/Puzzle/controller/attend/AttendControllerTest.java
@@ -6,6 +6,7 @@ import com.server.Puzzle.domain.attend.dto.request.PatchAttendRequest;
 import com.server.Puzzle.domain.attend.dto.response.FindAllAttendResponse;
 import com.server.Puzzle.domain.attend.enumtype.AttendStatus;
 import com.server.Puzzle.domain.attend.service.AttendService;
+import com.server.Puzzle.domain.board.enumType.IsAttendStatus;
 import com.server.Puzzle.global.enumType.Language;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,7 +18,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -46,9 +46,11 @@ public class AttendControllerTest {
 
     @Test
     void 참가요청() throws Exception {
+        // given
         doNothing().when(attendService)
                 .requestAttend(any(Long.class));
 
+        // when, then
         mockMvc.perform(
                     post("/api/attend/board/{boardId}",any(Long.class))
                 )
@@ -58,11 +60,13 @@ public class AttendControllerTest {
 
     @Test
     void 참가요청목록_전체조회() throws Exception {
+        // given
         FindAllAttendResponse response = findAllAttendResponse();
 
         doReturn(List.of(response)).when(attendService)
                 .findAllAttend(any(Long.class));
 
+        // when, then
         mockMvc.perform(
                     get("/api/attend/board/{boardId}",1L)
                 )
@@ -77,11 +81,13 @@ public class AttendControllerTest {
 
     @Test
     void 참가요청_상태_수정() throws Exception {
+        // given
         doNothing().when(attendService)
                         .patchAttend(any(Long.class),any(PatchAttendRequest.class));
 
         String body = patchAttendBody();
 
+        // when, then
         mockMvc.perform(
                     patch("/api/attend/{attendId}", 1L)
                             .content(body)
@@ -91,17 +97,32 @@ public class AttendControllerTest {
                 .andExpect(content().string("Success"));
     }
 
-
     @Test
     void 참가요청_취소() throws Exception {
+        // given
         doNothing().when(attendService)
                 .deleteAttend(any(Long.class));
 
+        // when, then
         mockMvc.perform(
                     delete("/api/attend/board/{boardId}", 1L)
                 )
                 .andExpect(status().isOk())
                 .andExpect(content().string("Success"));
+    }
+
+    @Test
+    void 참가요청_상태확인() throws Exception {
+        // given
+        doReturn(IsAttendStatus.CAN).when(attendService)
+                .checkAttendStatus(any(Long.class));
+
+        // when, then
+        mockMvc.perform(
+                        get("/api/attend/status/board/{boardId}", 1L)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().string("\"CAN\""));
     }
 
     private FindAllAttendResponse findAllAttendResponse() {
